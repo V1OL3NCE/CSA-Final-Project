c6935912ff13153338ee750371515c724840d047
JustinZheng <justinz121@nycstudents.net>
2026-06-02 10:06:16 -0400

    figured out some shi

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
+
 extends CharacterBody2D
-@export var speed: float = 300.0
+@export var speed: float = 30000.0
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * speed
+	velocity = direction * speed * delta
 	move_and_slide()
