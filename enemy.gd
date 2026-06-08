881f047a22f15779c4d759ee8dbf70a4321da1b4
MuntasirChowdhury <noname@bths>
2026-06-08 10:42:30 -0400

    enemySript

--- enemy.gd
+++ enemy.gd
@@ -1,1 +1,8 @@
 extends CharacterBody2D
+
+@export var speed = 90.0
+@onready var player = get_node("../Player") # Target node
+
+func _process(delta):
+	# Moves this sprite's position towards the player's position
+	global_position = global_position.move_toward(player.global_position, speed * delta)
