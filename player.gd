c9218b4b8721ee21abbc1414562e9dcde4b1c290
MuntasirChowdhury <noname@bths>
2026-06-12 10:20:14 -0400

    playerareaAdded

--- player.gd
+++ player.gd
@@ -1,8 +1,16 @@
 
 extends CharacterBody2D
 @export var speed: float = 10000.0
+var health : int = 100
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed * delta
 	move_and_slide()
+
+# This will trigger whenever another physics body enters the Area2D
+func _on_body_entered(body: CharacterBody2D) -> void:
+	# Check if the colliding object is the player
+	if body.is_in_group("Player"):
+		print("The player has entered the zone!")
+		# Add your destruction or game-state logic here
